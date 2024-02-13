2225bbc418595bd948778ce22a8f176f985cf51b
Mykhailo Yanitskyi <mykhailoyanitskyi@nerdzlab.com>
2024-02-13 17:16:47 +0200

    add OG image, code reafactore, game over page

--- src/pages/game-over.tsx
+++ src/pages/game-over.tsx
@@ -1,13 +1,39 @@
-import styles from "../styles/Game.module.css";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import styles from "../styles/Over.module.css";
 import { Routes } from "@/constants/router";
+
+function convertToFormattedNumber(amount: number): string {
+  return parseFloat(amount.toString()).toLocaleString("en-US", {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+}
 
 export default function GameOver() {
   const router = useRouter();
+
+  const totalScore = convertToFormattedNumber(1000);
+
   return (
-    <div className={styles.container}>
-      Game over
-      <button onClick={() => router.push(Routes.GAME)}>try again</button>
-    </div>
+    <main className={styles.main}>
+      <Image
+        src="/images/Hand.png"
+        width={624}
+        height={367}
+        alt="Picture of the author"
+      />
+      <div>
+        <h3 className={styles.subtitle}>Total score:</h3>
+        <h2 className={styles.title}>${totalScore} earned</h2>
+        <button
+          className="base"
+          role="link"
+          onClick={() => router.push(Routes.GAME)}
+        >
+          Try again
+        </button>
+      </div>
+    </main>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,26 +15,29 @@ export default function Home() {
     <>
       <Head>
         <title>Who Wants to Be a Millionaire?</title>
-        <meta name="description" content="Who Wants to Be a Millionaire?" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="One Million" />
+        <meta
+          property="og:description"
+          content="Who Wants to Be a Millionaire?"
+        />
+        <meta property="og:image" content="/open-graph.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <Image
-            src="/images/Hand.png"
-            width={624}
-            height={367}
-            alt="Picture of the author"
-          />
-          <div>
-            <h2 className={styles.title}>Who wants to be a millionaire?</h2>
-            <button className={styles.button} role="button" onClick={playGame}>
-              Start
-            </button>
-          </div>
+      <div className={styles.main}>
+        <Image
+          src="/images/Hand.png"
+          width={624}
+          height={367}
+          alt="Picture of the author"
+        />
+        <div>
+          <h2 className={styles.title}>Who wants to be a millionaire?</h2>
+          <button className="base" role="button" onClick={playGame}>
+            Start
+          </button>
         </div>
-      </main>
+      </div>
     </>
   );
 }
