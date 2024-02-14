0f5458f47233fdfdbac074a738728517d55feb42
Mykhailo Yanitskyi <mykhailoyanitskyi@nerdzlab.com>
2024-02-14 22:10:01 +0200

    remove todo

--- src/components/Confetti/ConfettiWrapper.tsx
+++ src/components/Confetti/ConfettiWrapper.tsx
@@ -4,10 +4,6 @@ import { useRecoilValue } from "recoil";
 import { useWindowSize } from "react-use";
 import { is1MillionReached } from "@/store/atoms";
 
-// todo: sound componemt, sound atom
-// todo: create hsaMap and change src
-// todo: based on recoil state play pause
-
 export function ConfettiWrapper({ children }: { children: React.ReactNode }) {
   const is1MillionReachedValue = useRecoilValue(is1MillionReached);
   const { width, height } = useWindowSize();
